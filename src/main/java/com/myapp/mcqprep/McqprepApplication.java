3377b4793861399eba52eb186888d23a10ec8f75
chiragkoli285 <chiragkoli285@gmail.com>
2026-08-29 12:17:20 +0530

    first commit

---
+++ src/main/java/com/myapp/mcqprep/McqprepApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.mcqprep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class McqprepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(McqprepApplication.class, args);
+	}
+
+}
